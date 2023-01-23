099ab26774a20de42165bdc2222e5e7ff3ca0a97
loic <loic.magne@outlook.com>
2023-01-23 18:08:12 +0100

    refactor+types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,1 +1,27 @@
 export type Theme = "light" | "dark";
+
+export interface MoleculeData {
+  inchi: string
+  name: string
+  psychonaut_names: string[] | null;
+  tripsit_names: string[] | null;
+  isomerd_names: string[] | null;
+  isod_ids: number[] | null;
+  hsdb_names: string[] | null;
+  cid: number[] | null;
+  sim: Array<SimilarMolecule>
+  synonyms: Array<string> | null
+  svg: string
+  search: boolean
+}
+
+export interface SimilarMolecule {
+  name: string
+  psychonaut_names: string[] | null;
+  tripsit_names: string[] | null;
+  isomerd_names: string[] | null;
+  isod_ids: number[] | null;
+  hsdb_names: string[] | null;
+  cid: number[] | null;
+  dist: number
+}
