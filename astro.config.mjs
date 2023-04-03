1b6bdbcb3ff38acf3d5798be503a50804ff97944
loicmagne <loic.magne@outlook.com>
2023-04-03 21:34:22 +0200

    robots.txt + sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import preact from '@astrojs/preact';
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-import remarkMath from "remark-math"
+import remarkMath from "remark-math";
 import rehypeMathJax from 'rehype-mathjax';
+import robotsTxt from "astro-robots-txt";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whitehatchemistry.github.io',
+  site: 'https://whitehatchem.github.io',
   integrations: [
     preact(),
     tailwind({
@@ -20,10 +18,12 @@ export default defineConfig({
         applyBaseStyles: false
       }
     }),
-    mdx()
+    mdx(),
+    robotsTxt(),
+    sitemap()
   ],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeMathJax],
+    rehypePlugins: [rehypeMathJax]
   }
 });
