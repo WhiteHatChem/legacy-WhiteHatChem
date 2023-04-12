67f7d88d1cb35d9efd8cfef987ad5ea80a9962d7
wd400 <87280280+wd400@users.noreply.github.com>
2023-04-12 16:39:25 +0200

    Create es.ts

---
+++ src/i18n/es.ts
@@ -0,0 +1,53 @@
+export default {
+'nps': 'Nuevas Sustancias Psicoactivas',
+
+'home.mission': "Proporcionamos soluciones de Deep Learning para identificar y analizar nuevas sustancias psicoactivas (NPS).",
+'home.our_mission': "Nuestra Misión",
+'home.latest': "Últimas Investigaciones",
+'home.explore': "Explorar",
+'home.why': "¿Por qué?",
+'home.discord': "Únete a la comunidad en Discord",
+'home.hero1': "Manteniéndonos al día con el",
+'home.hero2': "siempre cambiante",
+'home.hero3': "mundo de las NPS",
+'home.sent1': "Análisis de vanguardia",
+'home.sent2': "de los últimos descubrimientos",
+'home.sent3': "Desarrollamos herramientas de Deep Learning que pueden identificar y analizar NPS de manera rápida, precisa y rentable, para reducir la carga de la salud pública por el abuso de drogas de diseño.",
+'home.sent4': "El mercado de drogas ilícitas ha sido transformado por la proliferación de drogas de diseño producidas clandestinamente, diseñadas para imitar las acciones fisiológicas de drogas conocidas mientras evitan las leyes de control de drogas. La carga de la salud pública por el abuso de NPS obliga a los laboratorios toxicológicos, policiales y aduaneros a buscarlos en incautaciones y muestras biológicas. Sin embargo, la identificación de NPS emergentes es un desafío debido a la diversidad química de estas sustancias y la naturaleza efímera de su aparición en el mercado ilícito.",
+'home.sent5': "Ofrecemos una plataforma en línea de vanguardia y herramientas de Deep Learning que ayudan a identificar y analizar nuevas drogas de diseño. Utilizando una base de datos de moléculas conocidas y técnicas de Deep Learning de última generación, nuestros algoritmos pueden extrapolar a nuevas sustancias para predecir sus propiedades. Esta es una herramienta valiosa para las fuerzas del orden, los funcionarios de salud pública y los laboratorios que trabajan para mantenerse al día con el panorama siempre cambiante del mercado de drogas ilícitas.",
+
+'molecule.checkon': "Verificar en",
+'molecule.synonyms': "Sinónimos",
+'molecule.similarities': "Similitudes",
+'molecule.structural': "Estructural",
+'molecule.affinities': "Afinidades",
+'molecule.metabolism': "Metabolismo",
+'molecule.no_sim': "No hay similitudes disponibles.",
+'molecule.aff_tab1': "Afinidades similares",
+'molecule.aff_tab2': "Moléculas similares con baja afinidad dopaminérgica",
+'molecule.aff_tab3': "Perfil de Afinidades",
+'molecule.sent1': "Compuestos estructuralmente más similares. El porcentaje es una medida de cuán estructuralmente similares son esas moléculas, lo cual a menudo está fuertemente correlacionado con sus efectos.",
+'molecule.sent2': "No hay similitudes estructurales disponibles.",
+'molecule.sent3': "Similitudes calculadas en base a las afinidades de las moléculas con el sitio de acoplamiento de proteínas seleccionado.",
+'molecule.sent4': "Lista de compuestos que tienen las afinidades de sitios de acoplamiento más similares. El porcentaje es una medida de cuán similares son las afinidades de unión de las moléculas, lo que debería ser un indicador de cuán similarmente interactúan con el cerebro.",
+'molecule.sent5': "Lista de compuestos que tienen las afinidades de sitios de acoplamiento más similares y también tienen interacciones altas con receptores de dopamina. Esto es, por lo tanto, un indicador de cuánto estimula esta molécula los receptores de dopamina.",
+'molecule.sent6': "Afinidades de unión con una lista de 34 sitios de acoplamiento predefinidos. Estas afinidades se utilizan para calcular las similitudes de interacciones.",
+'molecule.sent7': "Interacción de esta molécula con el metabolismo",
+'molecule.sent8': "Reacciones que metabolizan esta molécula",
+'molecule.sent9': "Reacciones que el metabolismo produce a partir de esta molécula",
+
+'reaction.product': 'Producto',
+'reaction.enzymes': 'Enzimas',
+
+'layout.support_us': 'Apóyanos',
+'layout.search_nps': 'Buscar NPS',
+'layout.join_discord': 'Únete a nuestro Discord',
+'layout.compounds': 'Compuestos',
+'layout.about': 'Acerca de',
+
+'support.sent1': 'Si te gusta lo que hacemos, puedes apoyarnos haciendo una donación aquí',
+
+'blog.sent1': 'Aquí encontrarás una colección de artículos informativos e interesantes sobre diversos temas relacionados con nuestros últimos hallazgos para WHC.',
+
+'search.placeholder': 'Buscar compuestos'
+}
