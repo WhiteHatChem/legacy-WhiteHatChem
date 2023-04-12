772fc4b48a59a523232927082a95755239844bb8
wd400 <87280280+wd400@users.noreply.github.com>
2023-04-12 17:07:59 +0200

    Create de.ts

---
+++ src/i18n/de.ts
@@ -0,0 +1,53 @@
+export default {
+'nps': 'Neue psychoaktive Substanzen',
+
+'home.mission': "Wir bieten Deep-Learning-Lösungen zur Identifizierung und Analyse neuer psychoaktiver Substanzen (NPS).",
+'home.our_mission': "Unsere Mission",
+'home.latest': "Aktuelle Forschung",
+'home.explore': "Erkunden",
+'home.why': "Warum?",
+'home.discord': "Tritt der Community auf Discord bei",
+'home.hero1': "Schritt halten mit der",
+'home.hero2': "sich ständig verändernden",
+'home.hero3': "Welt der NPS",
+'home.sent1': "State-of-the-Art-Analyse",
+'home.sent2': "neuester Entdeckungen",
+'home.sent3': "Wir entwickeln Deep-Learning-Tools, die schnell, genau und kosteneffizient NPS identifizieren und analysieren können, um die öffentliche Gesundheitslast durch den Missbrauch von Designerdrogen zu reduzieren.",
+'home.sent4': "Der illegale Drogenmarkt wurde durch die Verbreitung von heimlich hergestellten Designerdrogen neu geformt. Diese sind darauf ausgelegt, die physiologischen Wirkungen bekannterer Drogenmissbräuche nachzuahmen und gleichzeitig Drogenkontrollgesetzen zu entgehen. Die öffentliche Gesundheitslast durch NPS-Missbrauch verpflichtet toxikologische, polizeiliche und zollamtliche Labore, sie in beschlagnahmten Beweismitteln und biologischen Proben zu untersuchen. Die Identifizierung neuer NPS ist jedoch aufgrund ihrer chemischen Vielfalt und der flüchtigen Natur ihres Auftretens auf dem illegalen Markt eine Herausforderung.",
+'home.sent5': "Wir bieten eine hochmoderne Online-Plattform und Deep-Learning-Tools zur Identifizierung und Analyse neuer Designerdrogen. Mithilfe einer Datenbank bekannter Moleküle und modernster Deep-Learning-Techniken können unsere Algorithmen auf neue Substanzen extrapolieren, um deren Eigenschaften vorherzusagen. Dies ist ein wertvolles Instrument für Strafverfolgungsbehörden, Gesundheitsbeamte und Labore, die sich bemühen, mit der sich ständig verändernden Landschaft des illegalen Drogenmarktes Schritt zu halten.",
+
+'molecule.checkon': "Prüfen auf",
+'molecule.synonyms': "Synonyme",
+'molecule.similarities': "Ähnlichkeiten",
+'molecule.structural': "Strukturell",
+'molecule.affinities': "Affinitäten",
+'molecule.metabolism': "Metabolismus",
+'molecule.no_sim': "Keine Ähnlichkeiten verfügbar.",
+'molecule.aff_tab1': "Ähnliche Affinitäten",
+'molecule.aff_tab2': "Ähnliche Moleküle mit geringer dopaminerger Affinität",
+'molecule.aff_tab3': "Affinitätsprofil",
+'molecule.sent1': "Strukturell ähnlichste Verbindungen. Der Prozentsatz ist ein Maß für die strukture lle Ähnlichkeit dieser Moleküle, die oft stark mit ihren Wirkungen korreliert ist.",
+'molecule.sent2': "Keine strukturellen Ähnlichkeiten verfügbar.",
+'molecule.sent3': "Ähnlichkeiten basierend auf Affinitäten von Molekülen mit ausgewählten Proteindockingstellen.",
+'molecule.sent4': "Liste der Verbindungen, die die ähnlichsten Dockingstellenaffinitäten haben. Der Prozentsatz ist ein Maß dafür, wie ähnlich die Bindungsaffinitäten der Moleküle sind, was ein Anhaltspunkt dafür sein sollte, wie ähnlich sie mit dem Gehirn interagieren.",
+'molecule.sent5': "Liste der Verbindungen, die die ähnlichsten Dockingstellenaffinitäten haben und auch hohe Dopaminrezeptor-Interaktionen aufweisen. Dies ist somit ein Indikator dafür, wie stark dieses Molekül die Dopaminrezeptoren stimuliert.",
+'molecule.sent6': "Bindungsaffinitäten mit einer Liste von 34 vordefinierten Dockingstellen. Diese Affinitäten werden verwendet, um die Interaktionsähnlichkeiten zu berechnen.",
+'molecule.sent7': "Wechselwirkung dieses Moleküls mit dem Stoffwechsel",
+'molecule.sent8': "Reaktionen, die dieses Molekül metabolisieren",
+'molecule.sent9': "Reaktionen, die der Stoffwechsel aus diesem Molekül produziert",
+
+'reaction.product': 'Produkt',
+'reaction.enzymes': 'Enzyme',
+
+'layout.support_us': 'Unterstütze uns',
+'layout.search_nps': 'Suche NPS',
+'layout.join_discord': 'Tritt unserem Discord bei',
+'layout.compounds': 'Verbindungen',
+'layout.about': 'Über uns',
+
+'support.sent1': 'Wenn Ihnen gefällt, was wir tun, können Sie uns durch eine Spende hier unterstützen',
+
+'blog.sent1': 'Hier finden Sie eine Sammlung informativer und interessanter Artikel zu verschiedenen Themen rund um unsere neuesten Erkenntnisse für WHC.',
+
+'search.placeholder': 'Suche Verbindungen'
+}
