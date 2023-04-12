50afe1118064a4b5122da689043b6111aa9ad1c6
loicmagne <loic.magne@outlook.com>
2023-04-12 04:40:09 +0200

    implemented i18n translation

--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -1,3 +1,15 @@
 export default {
+  'nps': 'Nouvelles Substances Psychoactives',
+
   'home.mission': "Nous proposons des solutions de Deep Learning qui permettent d'identifier et d'analyser les nouvelles substances psychoactives (NPS).",
+  'home.our_mission': "Notre Mission",
+  'home.latest': "Dernière recherches",
+  'home.explore': "Explorer",
+  'home.why': "Pourquoi ?",
+  'home.discord': "Rejoignez la communauté sur Discord",
+  'home.hero1': "Rester en phase avec le",
+  'home.hero2': "monde",
+  'home.hero3': "des NPS",
+
+  'molecule.checkon': "Détails sur",
 }
